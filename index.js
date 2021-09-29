d1ce908ce8e10adb7c13fc9cb835e1a117015f35
5n0weirdo <snoweirdo@protonmail.com>
2021-09-29 10:14:31 +0200

    Added functionality to when wrong or correct color is picked

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var colors = [
   "rgb(255, 0, 0)",
   "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
   "rgb(0, 255, 255)",
   "rgb(0, 0, 255)",
   "rgb(255, 0, 255)"
@@ -17,6 +18,15 @@ colorDisplay.textContent = pickedColor;
 
     // add click listeners to squares
     squares[i].addEventListener("click", function() {
-      alert("Clicked a square");
+      // grab color of clicked square
+      var clickedColor = this.style.backgroundColor;
+
+      // grab color to pickedColor
+      if(clickedColor === pickedColor) {
+        alert("Correct!");
+      } else {
+        alert("Wrong!");
+      }
     });
+
   }
