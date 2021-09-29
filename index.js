3433a772a62b91d66e869d0e7db8469d2627495b
5n0weirdo <snoweirdo@protonmail.com>
2021-09-29 11:53:52 +0200

    Reset colors functionality added

--- index.js
+++ index.js
@@ -24,6 +24,10 @@ resetButton.addEventListener("click", function() {
   // change colorDisplay to match pickedColor
   colorDisplay.textContent = pickedColor;
   // change colors of squares
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+  }
+
 })
 
 colorDisplay.textContent = pickedColor;
