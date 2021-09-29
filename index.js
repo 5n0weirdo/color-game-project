d5298bbf374329d0898a5e8a97e04397e46e6419
5n0weirdo <snoweirdo@protonmail.com>
2021-09-29 11:59:48 +0200

    'Play Again' button added

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ resetButton.addEventListener("click", function() {
   for(var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
-
+  h1.style.backgroundColor = "#232323";
 })
 
 colorDisplay.textContent = pickedColor;
@@ -43,8 +43,10 @@ colorDisplay.textContent = pickedColor;
       // compare color to pickedColor
       if(clickedColor === pickedColor) {
         messageDisplay.textContent = "Correct!";
+        resetButton.textContent = "Play Again?";
         changeColors(clickedColor);
         h1.style.backgroundColor = clickedColor;
+
       } else {
         this.style.backgroundColor = "#232323";
         messageDisplay.textContent = "Try Again.";
