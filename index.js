0abb5b36a9f63d025ca0213006c40a46e1495518
5n0weirdo <snoweirdo@protonmail.com>
2021-09-29 10:05:28 +0200

    Added an alert to when square is clicked

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ var colorDisplay = document.getElementById("colorDisplay");
 colorDisplay.textContent = pickedColor;
 
   for(var i = 0; i < squares.length; i++) {
+    // add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+
+    // add click listeners to squares
+    squares[i].addEventListener("click", function() {
+      alert("Clicked a square");
+    });
   }
