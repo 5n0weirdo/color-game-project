4cab7f9ca89c56653d6896ff0cc1861c529a51e7
5n0weirdo <snoweirdo@protonmail.com>
2021-09-29 10:00:38 +0200

    Changed the header to display RGB text color

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ var colors = [
   "rgb(255, 0, 255)"
 ]
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
 
+colorDisplay.textContent = pickedColor;
 
   for(var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
