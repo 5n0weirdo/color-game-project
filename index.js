a21713734cdf97fb7aa52c2088e9fdec037eaab0
5n0weirdo <snoweirdo@protonmail.com>
2021-09-29 11:49:06 +0200

    Added 'New Colors' button

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function() {
+  // generate all new colors
+  colors = generateRandomColors(6);
+  // pick a new random color from array
+  pickedColor = pickColor();
+  // change colorDisplay to match pickedColor
+  colorDisplay.textContent = pickedColor;
+  // change colors of squares
+})
 
 colorDisplay.textContent = pickedColor;
 
